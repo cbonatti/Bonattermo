534ba40002eff757bf9d268c5cb30c90c468b8aa
Carlos Bonatti <carlos.bonatti@live.com>
2022-04-17 01:07:02 -0300

    extracted home page

---
+++ Bonattermo/lib/home.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'action-button.dart';
+import 'clear-history-dialog.dart';
+import 'game/new-game.dart';
+import 'history/history-file.dart';
+import 'history/history.dart';
+import 'howToPlay.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BonaTTermo'),
+        actions: [
+          ActionButton.create(context, HowToPlayDialogBox(), Icons.info),
+          ActionButton.create(
+            context,
+            ConfirmClearHistoryDialogBox(
+              () async => {
+                await HistoryFile.deleteFile(),
+                setState(() {}),
+              },
+            ),
+            Icons.delete,
+          ),
+        ],
+      ),
+      body: History(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => NewGame().newGame(context),
+        tooltip: 'Novo Jogo',
+        child: Icon(Icons.play_arrow),
+      ),
+    );
+  }
+}
--- Bonattermo/lib/main.dart
+++ Bonattermo/lib/main.dart
@@ -1,11 +1,5 @@
-import 'package:bonattermo/action-button.dart';
-import 'package:bonattermo/howToPlay.dart';
 import 'package:flutter/material.dart';
-
-import 'clear-history-dialog.dart';
-import 'game/new-game.dart';
-import 'history/history.dart';
-import 'history/history-file.dart';
+import 'home.dart';
 
 void main() {
   runApp(MyApp());
@@ -20,44 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BonaTTermo'),
-        actions: [
-          ActionButton.create(context, HowToPlayDialogBox(), Icons.info),
-          ActionButton.create(
-            context,
-            ConfirmClearHistoryDialogBox(
-              () async => {
-                await HistoryFile.deleteFile(),
-                setState(() {}),
-              },
-            ),
-            Icons.delete,
-          ),
-        ],
-      ),
-      body: History(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => NewGame().newGame(context),
-        tooltip: 'Novo Jogo',
-        child: Icon(Icons.play_arrow),
-      ),
+      home: HomePage(),
     );
   }
 }
